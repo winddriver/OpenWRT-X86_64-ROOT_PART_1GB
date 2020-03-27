4e4f4d6281968227e15041927459990468d30b53
Souledge <winddriver@msn.com>
2020-03-27 10:58:05 +0800

    update config

--- diy.sh
+++ diy.sh
@@ -7,15 +7,15 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-echo "src-git lienol https://github.com/Lienol/openwrt-package" >> ./feeds.conf.default
-./scripts/feeds update -a
-rm -rf feeds/lienol/lienol/ipt2socks
-rm -rf feeds/lienol/lienol/shadowsocksr-libev
-rm -rf feeds/lienol/lienol/pdnsd-alt
-rm -rf feeds/lienol/package/verysync
-rm -rf feeds/lienol/lienol/luci-app-verysync
-rm -rf package/lean/luci-app-kodexplorer
-rm -rf package/lean/luci-app-pppoe-relay
-rm -rf package/lean/luci-app-pptp-server
-rm -rf package/lean/luci-app-v2ray-server
-./scripts/feeds install -a
+#echo "src-git lienol https://github.com/Lienol/openwrt-package" >> ./feeds.conf.default
+#./scripts/feeds update -a
+#rm -rf feeds/lienol/lienol/ipt2socks
+#rm -rf feeds/lienol/lienol/shadowsocksr-libev
+#rm -rf feeds/lienol/lienol/pdnsd-alt
+#rm -rf feeds/lienol/package/verysync
+#rm -rf feeds/lienol/lienol/luci-app-verysync
+#rm -rf package/lean/luci-app-kodexplorer
+#rm -rf package/lean/luci-app-pppoe-relay
+#rm -rf package/lean/luci-app-pptp-server
+#rm -rf package/lean/luci-app-v2ray-server
+#./scripts/feeds install -a
