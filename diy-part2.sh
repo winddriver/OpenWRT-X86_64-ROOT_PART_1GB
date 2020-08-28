40203469685150c2d7a4c63c741c4ead7399f9b6
Souledge <winddriver@msn.com>
2020-08-28 12:02:55 +0800

    update config

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,3 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
